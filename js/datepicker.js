a9625d34622edd17d8895fa3dbca49e235740c72
Fabrizio Regini <freegenie@gmail.com>
2014-02-07 11:11:44 +0100

    Capitalize days

--- js/datepicker.js
+++ js/datepicker.js
@@ -270,7 +270,7 @@
          * Locale text for day/month names: provide a hash with keys 'daysMin', 'months' and 'monthsShort'. Default english
          */
         locale: {
-          daysMin: ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'],
+          daysMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
           months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
           monthsShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
         },
